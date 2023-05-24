d9aec4f9510e167f41556ac7fd8a9fd36cc0490a
AhMaD36789 <ahmadsa28121999@gmail.com>
2023-05-24 15:49:21 +0000

    day-1-frond-end

--- server.js
+++ server.js
@@ -12,14 +12,10 @@ let PORT = 3002;
 // const apiKey = process.env.APIkey;
 // const client = new pg.Client(process.env.DATABASE_URL)
 
-server.get('/', HomeHandler);
+//server.get('/', HomeHandler);
 // server.get('*', DefaultHandler);
 
-server.get('/ammanhome', HomeAmmanHandler);
-server.get('/wadirummhome', HomeWadiRummHandler);
-server.get('/irbidhome', HomeIrbidHandler);
-server.get('/aqabahome', HomeAqabaHandler);
-server.get('/petrahome', HomePertaHandler);
+server.get('/', HomeHandler);
 
 server.get('/Listrestaurants', async function Listrestaurants(req, res) {
 
@@ -80,8 +76,6 @@ server.get('/RestaurantDetails', async function (req, res) {
 
 });
 
-
-
 server.post('/restaurants', async function (req, res) {
     const { city, longitude, latitude } = req.body;
 
@@ -114,16 +108,16 @@ server.post('/restaurants', async function (req, res) {
     }
 });
 
+ 
+async function HomeHandler(req, res){
 
-
-
-async function HomeAmmanHandler(req, res) {
+    const {lat,long} = req.query;
     const options = {
         method: 'GET',
         url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng',
         params: {
-            latitude: '31.945335',
-            longitude: '35.886671',
+            latitude:lat,
+            longitude:long,
             limit: '30',
             distance: '6',
             lunit: 'km',
@@ -167,220 +161,6 @@ async function HomeAmmanHandler(req, res) {
         res.status(500).send(error);
     }
 }
-
-
-
-async function HomeAqabaHandler(req, res) {
-    const options = {
-        method: 'GET',
-        url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng',
-        params: {
-            latitude: '29.52667',
-            longitude: '35.00778',
-            limit: '30',
-            distance: '8',
-            lunit: 'km',
-        },
-        headers: {
-            'X-RapidAPI-Key': '1a821d29fcmsh97ccbc06ad20fe8p1ec809jsn98de1f3b43cc',
-            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-        }
-    };
-
-    try {
-        const response = await axios.request(options);
-        const data = response.data.data;
-
-
-
-        let mapResult = data.map(item => {
-            if (item.photo && item.photo.images && item.photo.images.original) {
-                let singleresult = new AmmanRestaurant(
-                    item.name,
-                    item.photo.images.original.url,
-                    item.rating,
-                    item.distance,
-                    item.description,
-                    item.web_url,
-                    item.phone,
-                    item.website,
-                    item.address,
-                    item.cuisine,
-                    item.hours
-                );
-                return singleresult;
-
-
-            }
-        });
-
-        res.send(mapResult);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error);
-    }
-}
-
-
-
-async function HomeWadiRummHandler(req, res) {
-    const options = {
-        method: 'GET',
-        url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng',
-        params: {
-            latitude: '29.542474',
-            longitude: '35.394125',
-            limit: '30',
-            distance: '20',
-            lunit: 'km',
-        },
-        headers: {
-            'X-RapidAPI-Key': '1a821d29fcmsh97ccbc06ad20fe8p1ec809jsn98de1f3b43cc',
-            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-        }
-    };
-
-    try {
-        const response = await axios.request(options);
-        const data = response.data.data;
-
-
-
-        let mapResult = data.map(item => {
-            if (item.photo && item.photo.images && item.photo.images.original) {
-                let singleresult = new AmmanRestaurant(
-                    item.name,
-                    item.photo.images.original.url,
-                    item.rating,
-                    item.distance,
-                    item.description,
-                    item.web_url,
-                    item.phone,
-                    item.website,
-                    item.address,
-                    item.cuisine,
-                    item.hours
-                );
-                return singleresult;
-
-
-            }
-        });
-
-        res.send(mapResult);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error);
-    }
-}
-
-async function HomeIrbidHandler(req, res) {
-    const options = {
-        method: 'GET',
-        url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng',
-        params: {
-            latitude: '32.551445',
-            longitude: '35.851479',
-            limit: '30',
-            distance: '10',
-            lunit: 'km',
-        },
-        headers: {
-            'X-RapidAPI-Key': '1a821d29fcmsh97ccbc06ad20fe8p1ec809jsn98de1f3b43cc',
-            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-        }
-    };
-
-    try {
-        const response = await axios.request(options);
-        const data = response.data.data;
-
-
-
-        let mapResult = data.map(item => {
-            if (item.photo && item.photo.images && item.photo.images.original) {
-                let singleresult = new AmmanRestaurant(
-                    item.name,
-                    item.photo.images.original.url,
-                    item.rating,
-                    item.distance,
-                    item.description,
-                    item.web_url,
-                    item.phone,
-                    item.website,
-                    item.address,
-                    item.cuisine,
-                    item.hours
-                );
-                return singleresult;
-
-
-            }
-        });
-
-        res.send(mapResult);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error);
-    }
-}
-
-
-
-async function HomePertaHandler(req, res) {
-    const options = {
-        method: 'GET',
-        url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng',
-        params: {
-            latitude: '30.324270',
-            longitude: '35.462641',
-            limit: '30',
-            distance: '10',
-            lunit: 'km',
-        },
-        headers: {
-            'X-RapidAPI-Key': '1a821d29fcmsh97ccbc06ad20fe8p1ec809jsn98de1f3b43cc',
-            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-        }
-    };
-
-    try {
-        const response = await axios.request(options);
-        const data = response.data.data;
-
-
-
-        let mapResult = data.map(item => {
-            if (item.photo && item.photo.images && item.photo.images.original) {
-                let singleresult = new AmmanRestaurant(
-                    item.name,
-                    item.photo.images.original.url,
-                    item.rating,
-                    item.distance,
-                    item.description,
-                    item.web_url,
-                    item.phone,
-                    item.website,
-                    item.address,
-                    item.cuisine,
-                    item.hours
-                );
-                return singleresult;
-
-
-            }
-        });
-
-        res.send(mapResult);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error);
-    }
-}
-
-
-
-
 
 function AmmanRestaurant(name, photo, rating, distance, description, web_url, phone, website, address, cuisine, hours) {
     this.name = name;
